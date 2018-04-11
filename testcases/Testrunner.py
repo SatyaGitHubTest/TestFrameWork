d5ceed46b6354734f396e918020aedf93868bb93
Satya S <satyassn@gmail.com>
2018-04-11 16:14:42 +0530

    added logger object to testrunner.py

--- testcases/Testrunner.py
+++ testcases/Testrunner.py
@@ -6,6 +6,7 @@ currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentfram
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0,parentdir) 
 
+import logging
 from common.readhosts import *
 from common.readtestcases import *
 from common.readproperties import *
@@ -15,7 +16,23 @@ cwd = os.getcwd()
 os.chdir('TestFrameWork/testcases')
 cwd = os.getcwd()
 print(cwd)
-print('path to the script {}'.format(currentdir))
+
+stflogger = logging.getLogger(__name__)
+stflogger.setLevel(logging.DEBUG)
+loghandle1 = logging.StreamHandler()
+#loghandle1.setLevel(logging.WARNING)
+
+loghandle2 = logging.FileHandler('../logs/stflog.log', mode='a')
+#loghandle2.setLevel(logging.WARNING)
+
+frmt = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+loghandle1.setFormatter(frmt)
+loghandle2.setFormatter(frmt)
+
+stflogger.addHandler(loghandle1)
+stflogger.addHandler(loghandle2)
+
+stflogger.debug('----- ***** starting simple test ***** -----')
 
 h1 = HostsData("../config/host_list.conf")
 for key, value in h1.hosts.items():
