6ada02661861739e7ed8f2db073109476511bada
Satya S <satyassn@gmail.com>
2018-04-12 18:00:33 +0530

    saving logger changes

---
+++ testcases/__init__.py
@@ -0,0 +1,14 @@
+import logging
+
+logger = logging.getLogger(__name__)
+print(logger)
+logger.setLevel(logging.DEBUG)
+handler1 = logging.StreamHandler()
+handler2 = logging.FileHandler('../logs/stflog.log', mode='a')
+frmt = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler1.setFormatter(frmt)
+handler2.setFormatter(frmt)
+logger.addHandler(handler1)
+logger.addHandler(handler2)
+logger.info("logger in __init__ .py file")
+
